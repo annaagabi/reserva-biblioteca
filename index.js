a44323736e6c837c84117ade026e0175c1ca6b67
Anna Gabriela <124202500+annaagabi@users.noreply.github.com>
2023-12-04 14:04:14 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// config inicial
+const express = require('express')
+const app = express()
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const mongoose = require('mongoose')
+
+const cors = require('cors')
+app.use(cors())
+
+// Forma de ler JSON/ middlewares
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+app.use(express.json())
+
+// Rotas da API
+const kindleRoutes = require('./routes/kindleRoutes')
+const salaGrupoRoutes = require('./routes/salaGrupoRoutes')
+
+app.use('/salagrupo', salaGrupoRoutes)
+app.use('/kindle', kindleRoutes)
+
+// Rota/ Endpoint inicial
+app.get('/', (req,res) =>{
+    res.json({message: 'Testando API'})
+})
+
+// entregar uma porta
+// mongoose.connect( process.env.MONGO ) // conectar ao banco de dados
+mongoose.connect( 'mongodb+srv://'+ process.env.DB_LOGIN + '' + ':' + process.env.DB_PASSWORD + '@bookingteste.meol0nr.mongodb.net/?retryWrites=true&w=majority') // conectar ao banco de dados
+.then(() => {
+    console.log("Conectamos ao mongo DB")
+    app.listen(3000) // Vai ler a porta
+})
+.catch((err) => console.log(err))
